98871c6b1ee8a8d478eb0d5d34cf0b7959c0a183
linhletd <42126424+linhletd@users.noreply.github.com>
2019-11-10 11:03:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,23 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+var express = require('express');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var dotenv = require('dotenv').config();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var config = require('./configuration.js')
+var app = express();
 
-const cors = require('cors')
+// Basic Configuration 
+// app.use(express.static('public'))
+var port = process.env.PORT || 3000;
+app = config(app);
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
-
-app.use(cors())
-
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
-
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+/** this project needs a db !! **/ 
+// mongoose.connect(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on('open', function() {
+console.log('Mongoose connected.');
 });
-
-
-// Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
-
-// Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage
-
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
