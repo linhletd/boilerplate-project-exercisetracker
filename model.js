6340f266a83177bcc5a2000105d468b8749eb675
linhletd <42126424+linhletd@users.noreply.github.com>
2019-11-10 11:05:23 +0700

    Create model.js

---
+++ model.js
@@ -0,0 +1,21 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let ObjectId = Schema.Types.ObjectId;
+
+let UserSchema = new Schema({
+  username: {type: String},
+  userid: {type: String}
+});
+
+let ExerciseSchema = new Schema({
+  userid: {type: String},
+  description: {type: String},
+  duration:{type: Number},
+  date: {type: Date, 'default': Date.now}
+})
+
+module.exports = {
+  UserModel: mongoose.model('User',UserSchema),
+  ExerciseModel: mongoose.model('Exercise', ExerciseSchema)
+}
